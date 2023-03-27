e4d82a2b9c2f2218ef409d0f0c6d729d2ce13ebf
kosinevskiy <kosakvova@gmail.com>
2023-03-27 19:19:41 +0400

    121

--- register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -1,14 +1,23 @@
 package edu.javacourse.register.business;
 
 import edu.javacourse.register.dao.MarriageDao;
+import edu.javacourse.register.domain.MarriageCertificate;
+import edu.javacourse.register.rest.MarriageController;
 import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Setter
 public class MarriageManager
 {
-    private MarriageDao dao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
+    private MarriageDao marriageDao;
 
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
-        throw new UnsupportedOperationException("Unsupported");
+        LOGGER.info("findMarriageCertificate called");
+        MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
+        return  new MarriageResponse();
     }
 }
--- register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/MarriageDao.java
@@ -1,6 +1,23 @@
 package edu.javacourse.register.dao;
 
+import edu.javacourse.register.business.MarriageManager;
+import edu.javacourse.register.domain.MarriageCertificate;
+import edu.javacourse.register.view.MarriageRequest;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.EntityManager;
+
+@Setter
 public class MarriageDao
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageDao.class);
+    private EntityManager entityManager;
+    private String test;
+    public MarriageCertificate findMarriageCertificate(MarriageRequest marriageRequest){
+        LOGGER.info("findMarriageCertificate called:{}", test);
+        return null;
+    }
 
 }
