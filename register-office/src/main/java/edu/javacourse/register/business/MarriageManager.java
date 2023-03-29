f6fde2b477225c46a45a22ee3b6b41af7a596f15
kosinevskiy <kosakvova@gmail.com>
2023-03-29 16:05:02 +0400

    123

--- register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -8,8 +8,11 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-@Service
+@Service("marriageService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 @Setter
 public class MarriageManager
 {
