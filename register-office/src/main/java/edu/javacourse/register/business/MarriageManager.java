aa72256690364069310cf87435101c3ec75ea41d
kosinevskiy <kosakvova@gmail.com>
2023-03-31 15:56:43 +0400

    124_1

--- register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -3,6 +3,7 @@ package edu.javacourse.register.business;
 import edu.javacourse.register.dao.MarriageDao;
 import edu.javacourse.register.dao.PersonDao;import edu.javacourse.register.domain.MarriageCertificate;
 import edu.javacourse.register.domain.Person;
+import edu.javacourse.register.domain.PersonFemale;
 import edu.javacourse.register.domain.PersonMale;
 import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service("marriageService")
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
@@ -31,23 +33,43 @@ public class MarriageManager
     @Transactional
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called");
-        MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
+//        MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
         personDao.findPersons();
 
 
-        personDao.addPerson(getPerson());
-        personDao.addPerson(getPerson());
-        personDao.addPerson(getPerson());
+        personDao.addPerson(getPerson(1));
+        personDao.addPerson(getPerson(2));
+        MarriageCertificate mc = getMarriageCertificate();
+        marriageDao.saveAllAndFlush(mc);
+        marriageDao.findAll();
 
         return  new MarriageResponse();
     }
 
-    private static Person getPerson() {
-        Person m = new PersonMale();
+    private MarriageCertificate getMarriageCertificate(){
 
-        m.setFirstName("1");
-        m.setLastName("2");
-        m.setPatronymic("3");
+        MarriageCertificate mc = new MarriageCertificate();
+        mc.setIssueDate(LocalDate.now());
+        mc.setNumber("12345");
+        mc.setActive(true);
+
+        List<Person> persons = personDao.findPersons();
+        for (Person person : persons){
+            if (person instanceof PersonMale){
+                mc.setHusband((PersonMale)person );
+            }else{
+                mc.setWife((PersonFemale)person);
+            }
+        }
+        return mc;
+    }
+
+    private static Person getPerson(int sex) {
+        Person m = sex == 1 ? new PersonMale() : new PersonFemale();
+
+        m.setFirstName("1" + sex);
+        m.setLastName("2" + sex);
+        m.setPatronymic("3_" + sex);
         m.setDateOfBirth(LocalDate.of(1991,3,12));
         return m;
     }
--- register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -14,7 +14,6 @@ public class PersonDao {
 
      public List<Person> findPersons(){
          Query query = entityManager.createNamedQuery("Person.findPersons");
-         query.setParameter("personId", 1L);
          return query.getResultList();
      }
 
